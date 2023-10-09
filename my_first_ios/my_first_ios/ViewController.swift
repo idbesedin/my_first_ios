f8621edf341475b147cafe7f6fc95a5a963a94e0
Besedin Ivan <ivanbesedin.05@gmail.com>
2023-10-09 16:51:10 +0300

    Added structure of project

---
+++ my_first_ios/my_first_ios/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  my_first_ios
+//
+//  Created by Ivan on 09.10.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
